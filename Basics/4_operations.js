85fac5ec407e201ee8aa719ab1d3ef2f35604ef7
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-27 23:49:45 +0800

    added operations

---
+++ Basics/4_operations.js
@@ -0,0 +1,55 @@
+//addition
+var sum = 10+10;
+console.log("Sum: ", sum);
+
+//subtraction
+var difference = 45-33;
+console.log("Difference: ", difference);
+
+//multiplication
+var product = 8 * 10;
+console.log("Product: ", product);
+
+//division
+var quotient = 66/33;
+console.log("Quotient: ", quotient);
+
+/*
+    increment
+        - means add 1 to it
+    
+    decrement
+        - means subtract 1 to it
+*/
+
+//increment
+var myVar1 = 87;
+myVar1++;
+
+console.log("Increment: ", myVar1);
+
+//decrement
+
+var myVar2 = 11;
+myVar2--;
+console.log("Decrement: ", myVar2);
+
+
+//decimal numbers (floating point numbers)
+
+
+//multiplying decimals numbers:
+var prod = 2.0 * 2.5;
+console.log("Product of 2 decimals: ", prod);
+
+
+//divide decimal numbers: 
+var quot = 4.4/2.0;
+console.log("Quotient of 2 decimals: ", quot);
+
+
+//remainder: %
+
+var remainder; 
+remainder = 11%3;
+console.log("Modulo/Remainder of 2 numbers: ", remainder);

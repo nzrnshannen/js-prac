ef8aa0c22b1847d1bbef7bf934f92e51946f64fd
Shannen Nazareno <nzrnshannen@gmail.com>
2023-02-27 23:36:59 +0800

    added assignment-operator

---
+++ Basics/3_assignment-operator.js
@@ -0,0 +1,26 @@
+//declaration vs assignment
+
+//console.log() <-- allows you to see things on the console
+
+var a; //declaration
+var b = 2; //assigment
+
+console.log(a);
+
+a = 7; //asignment
+b = a; //assigning contents of a to b
+
+console.log(a);
+
+
+//initializing a value
+var a = 9;
+console.log(a);
+
+var a = 5;
+var b = 10;
+var c = "This is a ";
+
+a = a+1; //6
+b = b + 5; //15
+c = c + "string"; //This is a string <--- concatenated

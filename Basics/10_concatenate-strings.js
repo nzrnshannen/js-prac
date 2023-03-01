8922b3e046bff906f86a3d19b24a4b7dcca65d4b
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 23:11:46 +0800

    modified 9

--- Basics/10_concatenate-strings.js
+++ Basics/10_concatenate-strings.js
@@ -1,3 +1,14 @@
 var finalStr = "First string, " + "second string.";
 
 console.log(finalStr);
+
+//using += operator
+
+
+var ourStr = "I come first. ";
+ourStr += "I come second."; //ourStr = ourStr + "I come second.";
+
+var myStr = "This is the first sentence. ";
+myStr+= "This is the second sentence.";
+
+console.log(myStr);

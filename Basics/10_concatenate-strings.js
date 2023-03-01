acef39cd25b52dede7bc9113116f3b4774f8d1eb
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 23:36:33 +0800

    added 10

--- Basics/10_concatenate-strings.js
+++ Basics/10_concatenate-strings.js
@@ -1,14 +1,26 @@
-var finalStr = "First string, " + "second string.";
+var finalStr = "First string, " + "second string.\n";
 
 console.log(finalStr);
 
-//using += operator
-
-
+//using += operator:
 var ourStr = "I come first. ";
-ourStr += "I come second."; //ourStr = ourStr + "I come second.";
+ourStr += "I come second.\n"; //ourStr = ourStr + "I come second.";
 
 var myStr = "This is the first sentence. ";
-myStr+= "This is the second sentence.";
+myStr+= "This is the second sentence.\n";
 
 console.log(myStr);
+
+
+//constructing strings using variables:
+var myName = "Shannen";
+var fullName = "Hello, my name is " + myName + " and I love to code.\n";
+
+console.log(fullName);
+
+
+//appending string variables
+var anAdjective = "BS Information Systems student.\n";
+var ourStr = "I am a ";
+ourStr += anAdjective;
+console.log(ourStr);

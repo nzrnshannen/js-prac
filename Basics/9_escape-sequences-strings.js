1b2fd0da9ce00da821a4966323eb0cc37a34fa07
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 23:00:35 +0800

    added 9

---
+++ Basics/9_escape-sequences-strings.js
@@ -0,0 +1,17 @@
+/*
+    CODE OUTPUT
+    \' single quote
+    \" double quote
+    \\ backslash
+    \n newline
+    \r carriage return
+    \t tab
+    \b backspace
+    \f form feed
+
+*/
+
+
+var myStr = "First line\nSecond line\n\tThird line with tab";
+
+console.log(myStr);

770759fec6a71b4906dbdd487a18133619e65cc2
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 22:45:22 +0800

    added 7_escaping-literal-quotes-in-strings

---
+++ Basics/7_escaping-literal-quotes-in-strings.js
@@ -0,0 +1,12 @@
+
+//using escape character: use \"string-here\"
+
+/*
+    Instead of: 
+var myStr = "I am a "double quoted" string inside double quotes"
+   ...which is a wrong one, use \ 
+*/
+
+var myStr = "I am \"double quoted\" string inside \"double quotes\"";
+
+console.log(myStr);

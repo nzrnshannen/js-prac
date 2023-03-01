49aee80ff27644598f3845f452926773cba4dfb1
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 22:48:59 +0800

    added 8

---
+++ Basics/8_quoting-strings-with-single-quotes.js
@@ -0,0 +1,11 @@
+/*
+    Strings can be surrounded either with
+     single quotes: '<string-here>' or 
+     double quotes "<string-here>"
+*/
+
+var stringExample1 = 'Example string one'; //single quoted string
+var stringExample2 = "Example string two"; //double quoted string
+
+console.log(stringExample1);
+console.log(stringExample2);

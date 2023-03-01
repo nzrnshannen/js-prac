3aa0a3afbc542e507682bb4766d30cc2b19c5974
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-01 22:41:18 +0800

    added 6_declare-string-variables

---
+++ Basics/6_declare-string-variables.js
@@ -0,0 +1,15 @@
+//string literals
+
+var firstName = "Alan";
+var lastName = "Turning";
+
+
+var myFirstName = "Shannen";
+var myLastName = "Nazareno";
+
+
+firstName = "Clint";
+
+
+console.log(firstName);
+

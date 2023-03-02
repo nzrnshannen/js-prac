178865a0cddf8fa98360bc5634483549c5101db3
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-02 22:05:56 +0800

    modified 12

--- Basics/12_getting-character-using-index-no.js
+++ Basics/12_getting-character-using-index-no.js
@@ -1,3 +1,4 @@
+//getting the first character: use [0]
 var firstLetterOfFirstName = "";
 var firstName = "Shannen";
 
@@ -10,3 +11,13 @@ var lastName = "Nazareno";
 
 firstLetterOfLastName = lastName[0];
 console.log(firstLetterOfLastName);
+
+
+//getting the last character: use [name.length - 1];
+
+var str_len = "";
+var str = "Hello";
+
+str_len = str[str.length - 1];
+
+console.log("\nGetting the last character: " + str_len);

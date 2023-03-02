6566f6358b6716bae0c9066db8be34fa7cd1313e
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-02 22:33:38 +0800

    added 15

---
+++ Basics/14_arrays-multiple-values.js
@@ -0,0 +1,11 @@
+
+//Arrays and its values are stored in BRACKETS [ ]
+//values inside an array can be mixed
+
+var ourArray = ["Shannen", 20];
+
+console.log("First array: " + ourArray);
+
+var myArray = ["Callie", 1];
+
+console.log("Second array: "+ myArray);
---
+++ Basics/15_nested-arrays.js
@@ -0,0 +1,21 @@
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+//ourArray[0][0] = "the universe"
+//ourArray[0][1] = 42
+//ourArray[1][0] = "everything"
+//ourArray[1][1] = 101010
+
+var myArray = [["Shannen Nazareno", 20], ["Clint Englis", 21]];
+
+//myArray[0][0] = "Shannen Nazareno"
+//myArray[0][1] = 20;
+//myArray[1][0] = "Clint Englis"
+//myArray[1][1] = 21;
+
+//MODIFYING ARRAYS
+//tip: use their index number
+
+var arr = [1, 2, 3, 4, 5];
+arr[arr.length-1] = 100; //changing the value of the last element
+
+console.log(arr[arr.length-1]); //printing the last element of the array

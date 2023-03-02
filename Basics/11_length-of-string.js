92be6a5a2c8d6fe2064b7e8d62e5ae153f72d837
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-02 22:03:28 +0800

    added 11 and 12

---
+++ Basics/11_length-of-string.js
@@ -0,0 +1,11 @@
+var firstNameLen = 0;
+var firstName = "Shannen";
+
+firstNameLen = firstName.length;
+console.log(firstNameLen);
+
+var lastNameLen = 0;
+var lastName = "Nazareno";
+lastNameLen = lastName.length;
+
+console.log(lastNameLen);
---
+++ Basics/12_getting-character-using-index-no.js
@@ -0,0 +1,12 @@
+var firstLetterOfFirstName = "";
+var firstName = "Shannen";
+
+firstLetterOfFirstName = firstName[0];
+
+console.log(firstLetterOfFirstName);
+
+var firstLetterOfLastName = "";
+var lastName = "Nazareno";
+
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName);

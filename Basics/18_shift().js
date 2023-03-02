f3d78ff32b1564fdba74b829e9d7c8d19089bd8b
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-02 23:16:52 +0800

    added shift()

---
+++ Basics/18_shift().js
@@ -0,0 +1,13 @@
+/*
+    shift()
+    - removes the first element in the array
+
+    syntax: var removedElement = existingArr.shift();
+*/
+
+var ourArray = ["Shannen", "Clint", "Buttercup"];
+
+var removedElement = ourArray.shift();
+
+console.log("The removed element: " + removedElement);
+console.log("New array contents: " + ourArray);

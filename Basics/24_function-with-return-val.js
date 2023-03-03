4f56ef7736ad1fbd8fbc9436459c72327e97a4c7
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-03 21:55:34 +0800

    added 24

---
+++ Basics/24_function-with-return-val.js
@@ -0,0 +1,20 @@
+function minusSeven(num) //num is equals to 10
+{
+    return num - 7; //return 10 -7
+                   //          3
+                   //returns 3
+}
+
+console.log(minusSeven(10)); //would print 3
+
+
+//another example
+
+function timesFive(num) //num is equals to 5
+{
+    return num * 5; //return 5 * 5
+                    //        25
+                    //returns 25
+}
+
+console.log(timesFive(5)); //prints 25

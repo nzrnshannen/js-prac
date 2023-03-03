1c9094c40a68a29a096c6f7c52cfcab5cbff87b2
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-03 21:06:18 +0800

    added 20

---
+++ Basics/20_functions-with-parameters.js
@@ -0,0 +1,14 @@
+function exampleFunc(a, b) //a = 10, b = 5
+{
+    console.log("Difference of a and b: ", a-b); //10 - 5
+}
+
+exampleFunc(10, 5);
+
+
+function myFunc(a, b)
+{
+    console.log("Sum of a and b: ", a+b);
+}
+
+myFunc(10, 5);

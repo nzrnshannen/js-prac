c74b7976916d1aab559812d111193c2a810f0a0b
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-04 20:52:07 +0800

    added 28

---
+++ Basics/28_conditionals-if.js
@@ -0,0 +1,24 @@
+function funcExample(isItTrue)
+{
+    if(isItTrue)
+    {
+        return "Yes, it is true!";
+    }
+    
+    return "No, it is false";
+}
+
+
+//test this function with bool values: true, false
+function trueOrFalse(wasThatTrue)
+{
+    if(wasThatTrue)
+    {
+        return "Yes, that was true!";
+    }
+
+    return "No, that wasn't true.";
+
+}
+
+console.log(trueOrFalse(true)); //change parameters here

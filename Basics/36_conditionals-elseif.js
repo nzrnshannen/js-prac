0c4a6419d7f84e266ee1bdfdf8debbf575e559ef
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-04 22:29:23 +0800

    added 36

---
+++ Basics/36_conditionals-elseif.js
@@ -0,0 +1,17 @@
+function testFunc(val)
+{
+    if(val>10)
+    {
+        return "Greater than 10";
+    }
+    else if (val<5)
+    {
+        return "Smaller than 5";
+    }
+    else
+    {
+        return "Between 5 and 10";
+    }
+}
+
+console.log(testFunc(7));

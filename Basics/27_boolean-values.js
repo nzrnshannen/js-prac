6f89d89163c09f343f61b435e61516f3aa716800
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-04 20:47:28 +0800

    added 27

---
+++ Basics/27_boolean-values.js
@@ -0,0 +1,22 @@
+/*
+    VALUES OF BOOLEAN
+    true (on)
+    false (off)
+
+*/
+
+function boolFunc_false()
+{
+    return false;
+}
+
+function boolFunc_true()
+{
+    return true;
+}
+
+console.log(boolFunc_true());
+console.log(boolFunc_false());
+
+var falseVal = boolFunc_false();
+console.log(falseVal);

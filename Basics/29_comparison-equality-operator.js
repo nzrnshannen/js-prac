9d4bfeb42452a2e2b9782fb15f4572fcddb4f776
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-04 20:59:39 +0800

    added 29

---
+++ Basics/29_comparison-equality-operator.js
@@ -0,0 +1,21 @@
+/*
+    equality ( == ) operator
+        - for comparing two objects
+    
+    Q: Why don't we just use ( = )?
+    A: Remember, = is for ASSIGNING like:
+        var num = 16
+        const str = "Hello!"
+        So, it can't be used for comparison.
+*/
+
+function testEqual(val)
+{
+    if(val == 12) //10 == 12 IS FALSE; 10 and 12 are not equal so the code inside if statement won't be executed
+    {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testEqual(10));

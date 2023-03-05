cb69ec6e25375b3312813132cc2f1885a3260697
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-05 23:47:36 +0800

    added 43

---
+++ Basics/43_build-js-objects.js
@@ -0,0 +1,24 @@
+/*
+    OBJECTS
+    - acts like arrays but instead of using indexes,
+    we use PROPERTIES
+    - properties are denoted with "  ":
+    - values are after the : 
+    - they can be any datatype in JavaScript
+*/
+
+var ourDog = {
+    "name": "Camper",
+    "legs": 4, 
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+var myDog = {
+    "name": "Saneem",
+    "legs": 4,
+    "tails": 2,
+    "friends": [] //values of an object can be empty
+};
+
+

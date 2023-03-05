08df1fb4a85bfac70ecb72491653b86c93b910f9
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-05 23:53:39 +0800

    added 43 and 44

---
+++ Basics/43_accessing-object-properties-with-variables.js
@@ -0,0 +1,10 @@
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+var playerNumber = 16;
+var player = testObj[playerNumber]; //testObj[16]
+                                    //testObj[16] = "Montana"
+                                    //var player has the value of "Montana"
---
+++ Basics/44_accessing-objects.js
@@ -0,0 +1,31 @@
+//using dot notation
+
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+//bracket notation
+ // -if your properties have space in them
+
+
+var testObj_2 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water",
+};
+
+var entreeValue = testObj_2["an entree"];
+var drinkValue= testObj_2["the drink"];
+
+/*
+    Note:
+    1. You can use ' ' or " " in bracket notation
+    2. Case sensitive
+
+*/
+

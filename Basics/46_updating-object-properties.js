6d18c679d3e3cdadb1b89e0644665b554eefb621
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-06 19:13:26 +0800

    added 46

---
+++ Basics/46_updating-object-properties.js
@@ -0,0 +1,19 @@
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+var testObj = {
+    "name": "Coder",
+    "legs": 4, 
+    "tails": 1,
+    "friends": ["Nazareno family"]
+};
+
+//use . notation
+testObj.name = "Saneem"; //changing the name from Coder to Saneem
+console.log(testObj.name);

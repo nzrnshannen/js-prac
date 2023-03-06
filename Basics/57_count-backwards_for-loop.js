4bf35f953fcf4391eb8d102ecc6e18a775ab6c3a
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-06 21:02:21 +0800

    added 57

---
+++ Basics/57_count-backwards_for-loop.js
@@ -0,0 +1,19 @@
+var testArr = [];
+
+//decrementing by 1
+for(var i = 20; i>0; i--)
+{
+    testArr.push(i);
+}
+
+console.log("Decrementing by 1: ", testArr);
+
+//decrementing by 2
+
+var myArr = [];
+for (var i = 20; i>0; i-=2)
+{
+    myArr.push(i);
+}
+
+console.log("\nDecremeting by 2: ", myArr);

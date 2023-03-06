999a9cab0f54e6aeec95c1edebe744f3206367a7
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-06 19:15:44 +0800

    added 47

---
+++ Basics/47_add-new-properties-to-object.js
@@ -0,0 +1,11 @@
+var myDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+//object.new-property = "property-value";
+myDog.color = "Black";
+
+console.log(myDog.color);

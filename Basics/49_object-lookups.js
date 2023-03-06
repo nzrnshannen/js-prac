5964cc36e7a46bd10f330a1d92ca59dad0478533
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-06 19:31:19 +0800

    added 49

---
+++ Basics/49_object-lookups.js
@@ -0,0 +1,26 @@
+function phoneticLookup(val)
+{
+    var result = "";
+
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    };
+
+
+    result = lookup[val];
+
+    return result;
+}
+
+console.log(phoneticLookup("charlie"));
+
+/*
+    Instead of using switch statements, you can
+    also use objects! Try by converting switch
+    statements into objects.
+*/

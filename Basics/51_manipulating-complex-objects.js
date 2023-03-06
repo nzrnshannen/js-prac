816563e0990e83c71f7bc76163e834328e7dd2bd
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-06 19:47:00 +0800

    added 51

---
+++ Basics/51_manipulating-complex-objects.js
@@ -0,0 +1,33 @@
+var myMusic = [
+    { //myMusic[0] = { }
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats":[
+            "CD",
+            "8T",
+            "LP"
+        ],
+
+        "gold": true
+    },
+
+    { //myMusic[1] = { }
+        "artist": "Beau Carnes",
+        "title": "Cereal Man",
+        "release_year": 2003,
+        "formats": [
+            "Youtube video"
+        ],
+    }
+];
+
+/*
+    CLASSIFICATION OF ARRAYS AND OBJECTS
+
+    Object: { }
+        - curly braces
+    Arrays:  [ ]
+        - square brackets
+
+*/

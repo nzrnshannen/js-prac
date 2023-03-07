0ca31cf15f4b320d6e6cd04ff21603761d87eebc
Shannen Nazareno <nzrnshannen@gmail.com>
2023-03-07 20:53:52 +0800

    added 62

---
+++ Basics/62_parseInt-func.js
@@ -0,0 +1,16 @@
+/*
+    parseInt() function
+        - takes a string then parse (convert) it into integer
+        - if it fails to return an integer, it will return a NaN
+*/
+
+
+function convertFunc(str)
+{
+    return parseInt(str);
+}
+
+var output = convertFunc("90");
+
+console.log(output);
+
